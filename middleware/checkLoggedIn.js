b33c8dd218c31e679512ff028e279c1cf6651649
theperfectfuel <robert@theperfectfuel.com>
2018-07-12 18:38:46 -0700

    New Recipe route working with auth - checkLoggedIn middleware working - associating user with new recipe

--- middleware/checkLoggedIn.js
+++ middleware/checkLoggedIn.js
@@ -1,8 +1,10 @@
 const checkLoggedIn = (req, res, next) => {
+	console.log('checking user right now: ', req.user.id)
 	if(req.user) {
 		return next();
 	}
-	res.redirect('http://localhost:3000/login');
+	//res.redirect('http://localhost:3000/login');
+	res.send('we cannot find the user');
 }
 
 module.exports = checkLoggedIn;
