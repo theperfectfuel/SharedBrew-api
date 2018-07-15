e9559de0cc32f83acb37dab542d0333958fff26f
theperfectfuel <robert@theperfectfuel.com>
2018-07-14 23:54:12 -0700

    refactoring routes for es6 syntax...other small changes

--- routes/router.js
+++ routes/router.js
@@ -90,9 +90,9 @@ Router.get('/check-user', (req, res) => {
 //======================================
 // API RESOURCE ROUTES
 //======================================
-Router.get('/list-recipes', function(req, res) {
+Router.get('/list-recipes', (req, res) => {
 	// use mongoose to get all recipes in the database
-	Recipe.find(function(err, recipes) {
+	Recipe.find((err, recipes) => {
 		// if there is an error retrieving, send the error. nothing after res.send(err) will execute
 		if (err) {
 		    return res.send(err);
@@ -102,9 +102,9 @@ Router.get('/list-recipes', function(req, res) {
 	});
 });
 
-Router.get('/list-recipes/:recipeID', function(req, res) {
+Router.get('/list-recipes/:recipeID', (req, res) => {
 	// use mongoose to get one recipe in the database
-	Recipe.findById(req.params.recipeID, function(err, recipe) {
+	Recipe.findById(req.params.recipeID, (err, recipe) => {
 		// if there is an error retrieving, send the error. nothing after res.send(err) will execute
 		if (err) {
 		    return res.send(err);
@@ -114,11 +114,12 @@ Router.get('/list-recipes/:recipeID', function(req, res) {
 	});
 });
 
-Router.get('/shopping-lists', function(req, res) {
+Router.get('/shopping-lists', (req, res) => {
 	//var user_id = req.user.sub;
 	// use mongoose to get user's shopping lists in the database
 
-	ShoppingList.where('brewer', '5b3c21555db7cf270ac37e47').find(function(err, shoppingLists) {
+	ShoppingList.where('brewer', '5b3c21555db7cf270ac37e47')
+		.find((err, shoppingLists) => {
 		// if there is an error retrieving, send the error. nothing after res.send(err) will execute
 		if (err) {
 		    return res.send(err);
@@ -138,7 +139,7 @@ Router.get('/shopping-lists', function(req, res) {
 	});
 });
 
-Router.post('/new-recipe', checkLoggedIn, function(req, res) {
+Router.post('/new-recipe', checkLoggedIn, (req, res) => {
 
 	if (req.user.id) {
 		console.log('you made it here ', req.user.id);
@@ -162,7 +163,7 @@ Router.post('/new-recipe', checkLoggedIn, function(req, res) {
 	});
 	
 	recipe._brewer = req.user.id;
-	recipe.save(function(err, recipe) {
+	recipe.save((err, recipe) => {
 		if (err) {
 			res.status(500).send('An error occurred');
 			return console.log('error saving ', err);
@@ -174,10 +175,10 @@ Router.post('/new-recipe', checkLoggedIn, function(req, res) {
 	});
 });
 
-Router.post('/shopping-list/:recipeID', function(req, res) {
+Router.post('/shopping-list/:recipeID', (req, res) => {
 	var shoppingList = new ShoppingList(req.body);
 	shoppingList.brewer = req.user.sub;
-	shoppingList.save(function(err, shoppingList) {
+	shoppingList.save((err, shoppingList) => {
 		if (err) {
 			res.status(500).send('An error occurred');
 			return console.log('error saving ', err);
@@ -191,7 +192,7 @@ Router.post('/shopping-list/:recipeID', function(req, res) {
 //======================================
 // CATCH ALL ROUTE
 //======================================
-Router.get('*', function(req, res) {
+Router.get('*', (req, res) => {
 	res.sendFile(path.resolve('public/views/home.html'));
 });
 
